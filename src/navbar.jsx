6879dd2abb224972f162142be70844fc5d55ee78
Ken Udoudo <kendo302003@gmail.com>
2023-08-07 10:41:53 -0700

    li

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,15 @@
 const Navbar = () =>{
     return (
-
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+                <li>Resume</li>
+                <li>Project</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
     )
 }
 
