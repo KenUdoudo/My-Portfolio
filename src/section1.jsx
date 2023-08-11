ed7fb86e959f879d1b698e24128f3da9c3754bea
Ken Udoudo <kendo302003@gmail.com>
2023-08-11 06:53:51 -0700

    p tag

--- src/section1.jsx
+++ src/section1.jsx
@@ -3,6 +3,7 @@ const Sec1 = () =>{
         <div className="">
            <p>Hello</p>
            <h2>I'm <span className="text-orange-600">Kendo</span>, Web Developer</h2>
+           <p>Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
         </div>
     )
 }
