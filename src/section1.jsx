2e06dfe00e5c0c2e70dd16e7bf599bdf0a0478cb
Ken Udoudo <kendo302003@gmail.com>
2023-08-11 06:51:54 -0700

    text color

--- src/section1.jsx
+++ src/section1.jsx
@@ -2,7 +2,7 @@ const Sec1 = () =>{
     return (
         <div className="">
            <p>Hello</p>
-           <h2>I'm Kendo, Web Developer</h2>
+           <h2>I'm <span className="text-orange-600">Kendo</span>, Web Developer</h2>
         </div>
     )
 }
