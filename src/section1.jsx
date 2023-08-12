e3838fe076e4bbad9c8b36b7375ff4f2c70f8a92
Ken Udoudo <kendo302003@gmail.com>
2023-08-12 08:23:08 -0700

    font

--- src/section1.jsx
+++ src/section1.jsx
@@ -3,7 +3,7 @@ const Sec1 = () =>{
         <div>
         <div className="flex flex-col justify-center items-center">
            <p className="text-xl font-semibold">Hello</p>
-           <h2>I'm <span className="text-orange-600">Kendo</span>, Web Developer</h2>
+           <h2 className="text-4xl font-bold">I'm <span className="text-orange-600">Kendo</span>, Web Developer</h2>
         </div>
            <p>Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
         </div>
