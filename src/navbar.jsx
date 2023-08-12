0e6376017b4a8eec012f09348962b9cf08d588e1
Ken Udoudo <kendo302003@gmail.com>
2023-08-12 08:12:27 -0700

    center didnt work

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () =>{
     return (
-        <div className="bg-black border-0 rounded w-full">
+        <div className="flex bg-black border-0 rounded w-4/5 justify-center items-center">
             <ul className="flex flex-row gap-x-5 text-white justify-center items-center">
                 <li>Home</li>
                 <li>About</li>
