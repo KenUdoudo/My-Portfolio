243c6d592da46b4d8e91f1aa3e134c58827fb0fb
Ken Udoudo <kendo302003@gmail.com>
2023-08-12 08:18:48 -0700

    flex col

--- src/section1.jsx
+++ src/section1.jsx
@@ -1,6 +1,6 @@
 const Sec1 = () =>{
     return (
-        <div className="">
+        <div className="flex flex-col">
            <p>Hello</p>
            <h2>I'm <span className="text-orange-600">Kendo</span>, Web Developer</h2>
            <p>Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
