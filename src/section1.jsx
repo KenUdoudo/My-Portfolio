cbc837f6f87d9e6c5aa998d2c04af12c2fd88f30
Ken Udoudo <kendo302003@gmail.com>
2023-08-13 11:01:36 -0700

    hover

--- src/section1.jsx
+++ src/section1.jsx
@@ -5,7 +5,7 @@ const Sec1 = () =>{
            <h2 className="text-4xl font-semibold">I'm <span className="text-red-400">Kendo</span>, Web Developer</h2>
            <p className="mt-3">Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
            <div className="flex flex-row gap-x-5 border-black border-2 rounded-full px-3 py-1">
-            <button>Portfolio</button>
+            <button className="hover:bg-red-400">Portfolio</button>
             <button>Hire Me</button>
            </div>
         </div>
