533269238fabed3befee122b52cf5bae00e6db15
Ken Udoudo <kendo302003@gmail.com>
2023-08-13 10:55:57 -0700

    mt

--- src/section1.jsx
+++ src/section1.jsx
@@ -3,7 +3,7 @@ const Sec1 = () =>{
         <div className="flex flex-col justify-center items-center gap-y-5">
            <p className="text-xl font-semibold">Hello</p>
            <h2 className="text-4xl font-semibold">I'm <span className="text-red-400">Kendo</span>, Web Developer</h2>
-           <p className="mt-10">Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
+           <p className="mt-3">Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
            <div className="flex flex-row">
             <button>Portfolio</button>
             <button>Hire Me</button>
