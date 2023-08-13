5e2fd756fd37fc3cafb59cd1f1853ecfc40941da
Ken Udoudo <kendo302003@gmail.com>
2023-08-13 10:59:32 -0700

    border round

--- src/section1.jsx
+++ src/section1.jsx
@@ -4,7 +4,7 @@ const Sec1 = () =>{
            <p className="text-xl font-semibold">Hello</p>
            <h2 className="text-4xl font-semibold">I'm <span className="text-red-400">Kendo</span>, Web Developer</h2>
            <p className="mt-3">Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
-           <div className="flex flex-row gap-x-5 border-black border-2">
+           <div className="flex flex-row gap-x-5 border-black border-2 rounded-full">
             <button>Portfolio</button>
             <button>Hire Me</button>
            </div>
