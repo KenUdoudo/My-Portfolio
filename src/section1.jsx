d10ae06989a5e7a72b4379507886cec5e14860c5
Ken Udoudo <kendo302003@gmail.com>
2023-08-13 11:03:27 -0700

    hover effect done

--- src/section1.jsx
+++ src/section1.jsx
@@ -4,8 +4,8 @@ const Sec1 = () =>{
            <p className="text-xl font-semibold">Hello</p>
            <h2 className="text-4xl font-semibold">I'm <span className="text-red-400">Kendo</span>, Web Developer</h2>
            <p className="mt-3">Kendo's exceptional web design ensured our website's success. Highly recommended!</p>
-           <div className="flex flex-row gap-x-5 border-black border-2 rounded-full px-3 py-1">
-            <button className="hover:bg-red-400">Portfolio</button>
+           <div className="flex flex-row gap-x-5 border-black border-2 rounded-full px-1 py-1">
+            <button className="hover:bg-red-400 px-2 rounded-full hover:text-white">Portfolio</button>
             <button>Hire Me</button>
            </div>
         </div>
