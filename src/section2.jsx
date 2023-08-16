e1dcfdbf0b9eb1b33ee247073b3d211fb952d5fb
Ken Udoudo <kendo302003@gmail.com>
2023-08-15 18:54:35 -0700

    lorem

--- src/section2.jsx
+++ src/section2.jsx
@@ -3,6 +3,7 @@ const Sec2 = () => {
         <div className="bg-black">
             <div>
                 <p>My <span>Services</span></p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolorum consequatur ratione accusantium illum. Blanditiis quas voluptate incidunt quae totam, voluptates soluta explicabo inventore vel, facilis modi sed est sit?</p>
             </div>
         </div>
     );
