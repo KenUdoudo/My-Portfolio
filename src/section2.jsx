7824f3ff231de95f1ca3fb84e4764bb702106fc2
Ken Udoudo <kendo302003@gmail.com>
2023-08-15 18:57:35 -0700

    margin

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,6 +1,6 @@
 const Sec2 = () => {
     return (  
-        <div className="bg-black">
+        <div className="bg-black mx-32">
             <div>
                 <p>My <span>Services</span></p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolorum consequatur ratione accusantium illum. Blanditiis quas voluptate incidunt quae totam, voluptates soluta explicabo inventore vel, facilis modi sed est sit?</p>
