708e9f7589a364ba8953e0e1a64b78801e457eb5
Ken Udoudo <kendo302003@gmail.com>
2023-08-15 18:58:57 -0700

    text color

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,8 +1,8 @@
 const Sec2 = () => {
     return (  
-        <div className="bg-black mx-32">
+        <div className="bg-black mx-32 text-white">
             <div>
-                <p>My <span>Services</span></p>
+                <p>My <span className="text-red-400">Services</span></p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolorum consequatur ratione accusantium illum. Blanditiis quas voluptate incidunt quae totam, voluptates soluta explicabo inventore vel, facilis modi sed est sit?</p>
             </div>
         </div>
