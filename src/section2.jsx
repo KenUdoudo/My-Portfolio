3986ce45265e9b6c91f1761ba97ffe3aea73b3f8
Ken Udoudo <kendo302003@gmail.com>
2023-08-15 19:03:52 -0700

    rounded border

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,6 +1,6 @@
 const Sec2 = () => {
     return (  
-        <div className="bg-black mx-32 text-white h-96">
+        <div className="bg-black mx-32 text-white h-96 rounded-md">
             <div className="flex flex-row">
                 <p>My <span className="text-red-400">Services</span></p>
                 <p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolorum consequatur ratione accusantium illum.</p>
