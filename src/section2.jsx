8872280560e72133f6172e8bb6c1f560012f06f5
Ken Udoudo <kendo302003@gmail.com>
2023-08-15 19:00:54 -0700

    text size

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,9 +1,9 @@
 const Sec2 = () => {
     return (  
         <div className="bg-black mx-32 text-white">
-            <div>
+            <div className="flex flex-row">
                 <p>My <span className="text-red-400">Services</span></p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolorum consequatur ratione accusantium illum. Blanditiis quas voluptate incidunt quae totam, voluptates soluta explicabo inventore vel, facilis modi sed est sit?</p>
+                <p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolorum consequatur ratione accusantium illum.</p>
             </div>
         </div>
     );
