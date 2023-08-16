e60850f71f426c4b3212f5ecaf5a4f94c087a79a
Ken Udoudo <kendo302003@gmail.com>
2023-08-15 18:55:52 -0700

    importing sec 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './navbar'
 import Sec1 from './section1'
+import Sec2 from './section2'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Navbar />
       
       <Sec1 />
+
+      <Sec2 />
     </div>
   )
 }
