16db0957cda8d89f5959344a44863c736cc1528b
Ken Udoudo <kendo302003@gmail.com>
2023-08-15 18:53:50 -0700

    services

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,7 +1,9 @@
 const Sec2 = () => {
     return (  
         <div className="bg-black">
-
+            <div>
+                <p>My <span>Services</span></p>
+            </div>
         </div>
     );
 }
