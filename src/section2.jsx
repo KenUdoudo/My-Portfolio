e0dead5ae104a0044f47365f473f1fa69bcd0b73
Ken Udoudo <kendo302003@gmail.com>
2023-08-18 13:14:28 -0700

    gap x

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,7 +1,7 @@
 const Sec2 = () => {
     return (  
         <div className="bg-black mx-32 text-white h-96 rounded-md">
-            <div className="flex flex-row gap-20">
+            <div className="flex flex-row gap-x-20">
                 <p>My <span className="text-red-400">Services</span></p>
                 <p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum dolorum consequatur ratione accusantium illum.</p>
             </div>
