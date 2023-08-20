a5ca1c4a624ea8b3bb481a20aaa1b0db4786802a
Ken Udoudo <kendo302003@gmail.com>
2023-08-19 23:34:21 -0700

    width

--- src/section2.jsx
+++ src/section2.jsx
@@ -1,6 +1,6 @@
 const Sec2 = () => {
     return (  
-        <div className="bg-black mx-32 text-white h-96 rounded-md">
+        <div className="bg-black mx-32 text-white h-96 w-4/5 rounded-md">
             <div className="flex flex-row gap-x-20">
                 <p>My <span className="text-red-400">Services</span></p>
                 <p className="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br /> Earum dolorum consequatur ratione accusantium illum.</p>
