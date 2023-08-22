409bc5b31731d8abbd7f13242b6e6b3e572fbd0e
Ken Udoudo <kendo302003@gmail.com>
2023-08-22 07:55:06 -0700

    importing sec 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './navbar'
 import Sec1 from './section1'
 import Sec2 from './section2'
+import Sec3 from './section3'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Sec1 />
 
       <Sec2 />
+
+      <Sec3 />
     </div>
   )
 }
