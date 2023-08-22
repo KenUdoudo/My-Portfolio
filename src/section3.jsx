bf656b829713f774690f864751dec841eb3126f3
Ken Udoudo <kendo302003@gmail.com>
2023-08-22 07:55:42 -0700

    text align

--- src/section3.jsx
+++ src/section3.jsx
@@ -1,7 +1,7 @@
 const Sec3 = () => {
     return (  
         <div className="mt-10">
-            <h2>My Work Experience</h2>
+            <h2 className="text-center">My Work Experience</h2>
             <div></div>
         </div>
     );
