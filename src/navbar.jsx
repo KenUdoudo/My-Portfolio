aaa92fd164dabb2608261e26c948bb7f1c0a563f
Ken Udoudo <kendo302003@gmail.com>
2023-08-24 03:08:17 -0700

    font thickness

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () =>{
     return (
         <div className="w-4/5">
-            <ul className="flex flex-row gap-x-5 text-gray-400 justify-center items-center p-4 mt-10">
+            <ul className="flex flex-row gap-x-5 text-gray-400 justify-center items-center p-4 mt-10 font-extrabold">
                 <a href="#"><li>HOME</li></a>
                 <a href="#"><li>ABOUT</li></a>
                 <a href="#"><li>BLOG CATEGORIES</li></a>
