31691eeba6c46cce289723757b63c943e735805e
Ken Udoudo <kendo302003@gmail.com>
2023-08-24 03:06:18 -0700

    removing bg

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () =>{
     return (
-        <div className="bg-black border-0 rounded w-4/5">
+        <div className="w-4/5">
             <ul className="flex flex-row gap-x-5 text-gray-400 justify-center items-center p-4 mt-10">
                 <a href="#"><li>HOME</li></a>
                 <a href="#"><li>ABOUT</li></a>
