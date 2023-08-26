4c42f7eb60cff53ef1ac413fc85d25741e877e59
Ken Udoudo <kendo302003@gmail.com>
2023-08-25 22:47:39 -0700

    commit

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,13 +1,20 @@
+import logo from './assets/plenti_logo_dark.png'
+
 const Navbar = () =>{
     return (
-        <div className="w-4/5">
-            <ul className="flex flex-row gap-x-5 text-gray-400 justify-center items-center p-4 mt-10 font-extrabold text-lg">
-                <a href="#"><li>HOME</li></a>
-                <a href="#"><li>ABOUT</li></a>
-                <a href="#"><li>BLOG CATEGORIES</li></a>
-                <a href="#"><li>VIDEOS</li></a>
-                <a href="#"><li className="text-red-800">GAME<span className="text-blue-800">RANX</span></li></a>
+        <div className="">
+           <img src={ logo }/>
+            <ul>
+                <li>For Merchants</li>
+                <li>Deals</li>
+                <li>How it works</li>
+                <li>Contact</li>
             </ul>
+            <div>
+                <button>Log in</button>
+                <button>Get started</button>
+            </div>
+           
         </div>
     )
 }
