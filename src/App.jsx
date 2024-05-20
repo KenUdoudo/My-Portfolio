6713831899bb5b449c9015a9bdd60a48addd2f89
Ken Udoudo <kendo302003@gmail.com>
2024-05-20 00:30:20 -0700

    clearing out

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './navbar'
-import Sec1 from './Home/section1'
-import Sec2 from './Home/section2'
-import Sec3 from './Home/section3'
-import Sec4 from './Home/section4'
-import Sec5 from './Home/section5'
-import FAQ from './Home/FAQs'
 
 function App() {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <Navbar />
+    <div>
       
-      <Sec1 />
-
-      <Sec2 />
-
-      <Sec3 />
-
-      <Sec4 />
-
-      <Sec5 />
-
-      <FAQ />
     </div>
   )
 }
