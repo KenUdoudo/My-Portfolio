04a282f480d36cc082dee247e064a1abc056f73d
Ken Udoudo <kendo302003@gmail.com>
2024-05-20 01:34:43 -0700

    started nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,14 @@
-
+import logo from '../assets/blacklogo.svg';
 
 const Navbar = () =>{
     return (
-        <div>
-           
+        <div className="absolute top-[10px] px-[10px]">
+            <div className='bg-white px-[20px] py-[7px] rounded-xl'>
+             <div className='flex flex-row items-center gap-[10px]'>
+                <img src={ logo } className='w-[50px] h-[50px]'/>
+                <p className='text-[#1b6535] text-[24px] font-bold'>NACOS MTU</p>
+             </div>
+           </div>
         </div>
     )
 }
