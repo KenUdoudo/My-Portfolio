57f3ec925c9a1167ddc2968333b9bc7d5b91c7f9
Ken Udoudo <kendo302003@gmail.com>
2024-05-21 23:27:38 -0700

    line height

--- src/pages/Home/section3.jsx
+++ src/pages/Home/section3.jsx
@@ -21,28 +21,28 @@ const Sec3home = () => {
             <div className="flex flex-row w-full h-[500px] gap-x-[10px] mt-[50px]">
                 <div className={`${tab === 0 ? "w-[60%]" : "w-[30%]"}  transition-all duration-500 relative`} onClick={()=> setTab(0)}>
                     <img src={ one } className={`${tab === 0 ? "" : "filter grayscale"}  object-cover w-full h-full`}/>
-                    <div className={`${tab === 0 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500`}>
+                    <div className={`${tab === 0 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500 leading-[1]`}>
                         <p className="text-[20px] font-medium">Merit Mohammed</p>
                         <p className="text-[30px] font-semibold">President</p>
                     </div>
                 </div>
                 <div className={`${tab === 1 ? "w-[60%]" : "w-[30%]"}  transition-all duration-500 relative`} onClick={()=> setTab(1)}>
                     <img src={ two } className={`${tab === 1 ? "" : "filter grayscale"}  object-cover w-full h-full`}/>
-                    <div className={`${tab === 1 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500`}>
+                    <div className={`${tab === 1 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500 leading-[1]`}>
                         <p className="text-[20px] font-medium">Damilola Adesina</p>
                         <p className="text-[30px] font-semibold">Vice President</p>
                     </div>
                 </div>
                 <div className={`${tab === 2 ? "w-[60%]" : "w-[30%]"}  transition-all duration-500 relative`} onClick={()=> setTab(2)}>
                     <img src={ three } className={`${tab === 2 ? "" : "filter grayscale"}  object-cover w-full h-full`}/>
-                    <div className={`${tab === 2 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500`}>
+                    <div className={`${tab === 2 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500 leading-[1]`}>
                         <p className="text-[20px] font-medium">Oluwayomi Apata</p>
                         <p className="text-[30px] font-semibold">Gen. Secretary</p>
                     </div>
                 </div>
                 <div className={`${tab === 3 ? "w-[60%]" : "w-[30%]"}  transition-all duration-500 relative`} onClick={()=> setTab(3)}>
                     <img src={ four } className={`${tab === 3 ? "" : "filter grayscale"}  object-cover w-full h-full`}/>
-                    <div className={`${tab === 3 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500`}>
+                    <div className={`${tab === 3 ? "text-black" : "text-white text-center"} absolute bottom-[20px] left-[20px] transition-all duration-500 leading-[1]`}>
                         <p className="text-[20px] font-medium">Ikemefuna Daniel</p>
                         <p className="text-[30px] font-semibold">Fin. Secretary</p>
                     </div>
--- src/pages/Home/section4.jsx
+++ src/pages/Home/section4.jsx
@@ -1,6 +1,6 @@
 const Sec4home = () => {
     return (  
-        <div className="px-[50px] mt-[100px]">
+        <div className="px-[50px] mt-[100px] bg-[#4b4a4a]">
             <div className="flex flex-row justify-between items-center w-full">
                 <div className='flex flex-row items-center gap-[10px]'>
                     <div className='h-[50px] w-[50px] bg-[#d7fec8] rounded-full shadow-xl'></div>
@@ -12,7 +12,16 @@ const Sec4home = () => {
                 <div className='h-[50px] w-[50px] bg-[#1b6535] rounded-full shadow-xl cursor-pointer hover:bg-[#30854e] transition-all duration-500'></div>
             </div>
 
-            
+            <div className="flex flex-row items-center overflow-x-hidden scroll-m-4">
+                <div>
+                    <div className="w-[300px] h-[170px] shadow-2xl rounded-xl bg-white relative">
+                        <div className="absolute bottom-[10px] flex flex-row justify-between items-center w-full  px-[10px]">
+                            <p className="leading-[0.9]"><span className="text-[18px] font-bold">16th</span><br />January</p>
+                            <p className="text-[17px] text-right font-bold leading-[0.9]">Freshers<br />Orientation</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
