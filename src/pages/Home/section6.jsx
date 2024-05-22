73c9e99a02d9f50663278da2b8cc736050dcc94f
Ken Udoudo <kendo302003@gmail.com>
2024-05-22 11:18:42 -0700

    margin

--- src/pages/Home/section6.jsx
+++ src/pages/Home/section6.jsx
@@ -1,3 +1,4 @@
+import logo from './assets/whitelogo.png';
 const Sec6home = () => {
     return (  
         <div className="px-[50px] mt-[100px]">
@@ -11,9 +12,20 @@ const Sec6home = () => {
                 </div>
             </div> 
 
-            <div className="w-full bg-black pt-[30px] pb-[20px] pl-[40px] pr-[30px]">
-                <div>
-                    
+            <div className="w-full bg-black pt-[30px] pb-[20px] pl-[40px] pr-[30px] mt-[30px]">
+                <div className='w-full flex flex-row justify-between items-center pr-[50px]'>
+                    <p className="text-[13px] text-white"><span className="text-[#e9e9e971]">../csc/nascos/</span>hackathon2024</p>
+                    <img src={ logo }/>
+                </div>
+
+                <div className="flex flex-col justify-start items-start w-full my-[70px]">
+                            <p className='text-[45px] text-white font-bold'>Ctrl + Alt + Compete</p>
+                            <p className='text-[30px] text-white font-light'>Show workings, guy!</p>    
+                </div>
+
+                <div className="flex flex-col-reverse justify-end items-end w-full leading-[1]">
+                            <p className=' text-white font-extrabold'>Coming Soon</p>
+                            <p className='text-[13px] text-white font-light'>An in-house Hackathon Event</p>    
                 </div>
             </div>
         </div>
