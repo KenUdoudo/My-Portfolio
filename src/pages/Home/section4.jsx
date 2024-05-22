05c864f85b5e2fc99fc2ebd5cdf5c24728647d51
Ken Udoudo <kendo302003@gmail.com>
2024-05-21 23:32:51 -0700

    omo

--- src/pages/Home/section4.jsx
+++ src/pages/Home/section4.jsx
@@ -12,12 +12,18 @@ const Sec4home = () => {
                 <div className='h-[50px] w-[50px] bg-[#1b6535] rounded-full shadow-xl cursor-pointer hover:bg-[#30854e] transition-all duration-500'></div>
             </div>
 
-            <div className="flex flex-row items-center overflow-x-hidden scroll-m-4">
-                <div>
-                    <div className="w-[300px] h-[170px] shadow-2xl rounded-xl bg-white relative">
+            <div className="flex flex-row items-center overflow-x-hidden scroll-m-4 mt-[30px]">
+                <div className="flex flex-col gap-[20px]">
+                    <div className="w-[330px] h-[200px] shadow-2xl rounded-xl bg-white relative">
                         <div className="absolute bottom-[10px] flex flex-row justify-between items-center w-full  px-[10px]">
-                            <p className="leading-[0.9]"><span className="text-[18px] font-bold">16th</span><br />January</p>
-                            <p className="text-[17px] text-right font-bold leading-[0.9]">Freshers<br />Orientation</p>
+                            <p className="leading-[0.9]"><span className="text-[22px] font-bold">16th</span><br />January</p>
+                            <p className="text-[22px] text-right font-bold leading-[0.9]">Freshers<br />Orientation</p>
+                        </div>
+                    </div>
+                    <div className="w-[330px] h-[300px] shadow-2xl rounded-xl bg-white relative">
+                        <div className="absolute bottom-[10px] flex flex-row justify-between items-center w-full  px-[10px]">
+                            <p className="leading-[0.9]"><span className="text-[22px] font-bold">3rd</span><br />February</p>
+                            <p className="text-[22px] text-right font-bold leading-[0.9]">EXAM WEEK</p>
                         </div>
                     </div>
                 </div>
