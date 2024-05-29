2bd2f58d62ad4b468b4f931acaf1b118b60da1cb
Ken Udoudo <kendo302003@gmail.com>
2024-05-29 07:21:22 -0700

    DONE WITH ROUTES

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Footer from './components/footer';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
 
+
 function App() {
   return (
-    <div className='relative'>
-      <div className='bg-[url(./assets/NNL2.svg)] bg-cover bg-no-repeat backdrop-opacity-25'>
-      <Navbar />
-
-      <Home />
-
-      <Footer />
+    <Router>
+      <div className='relative'>
+        <div className='bg-[url(./assets/NNL2.svg)] bg-cover bg-no-repeat backdrop-opacity-25'>
+        <Navbar />
+        <div className='content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
