4a756df68c07f1516228bb9806ca4f8a7e10b4e2
Ken Udoudo <kendo302003@gmail.com>
2024-05-29 07:11:25 -0700

    done

--- src/pages/Home/section1.jsx
+++ src/pages/Home/section1.jsx
@@ -12,12 +12,13 @@ const Sec1home = () => {
                 <p className='text-[30px] text-white'>Empowered to Excel</p>
                 <p className='bg-[#1b6535] text-white py-[10px] px-[30px] cursor-pointer hover:bg-[#30854e] rounded-xl transition-all duration-500'>Learn About</p>
             </div>
-            <div className='absolute bottom-[10px] left-[50px]'>
-                <div className='flex flex-row items-center'>
+            <div className='flex flex-row items-center gap-[15px] absolute bottom-[10px] left-[50px]'>
+                <div className='flex flex-row items-center gap-[10px]'>
                     <img src={ greenlogo } className='w-[50px] aspect-w-full'/>
-                    <span className='w-[5px] h-full bg-white'></span>
+                    <span className='w-[3px] h-[50px] bg-white'></span>
                     <img src={ schoollogo } className='w-[50px] aspect-w-full'/>
                 </div>
+                <p className='text-white text-[14px] leading-[0.9]'>IN ASSOCIATION WITH <br />THE NIGERIAN ASSOCIATION OF COMPUTING STUDENTS <br />AND MOUNTAIN TOP UNIVERSITY</p>
             </div>
             </div>
 
