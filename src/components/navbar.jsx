8d0a6d3d7931e73c86fa2ccde828e43505d0cc74
Ken Udoudo <kendo302003@gmail.com>
2024-05-29 07:32:14 -0700

    toggle btn

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,11 @@
 import logo from '../assets/blacklogo.svg';
-
+import { HiOutlineMenu } from "react-icons/hi";
 const Navbar = () =>{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <div className="absolute top-[10px] px-[10px] w-full z-20">
             <div className='bg-white px-[40px] py-[7px] rounded-xl flex flex-row justify-between items-center'>
@@ -9,7 +14,9 @@ const Navbar = () =>{
                 <p className='text-[#1b6535] text-[24px] font-bold'>NACOS MTU</p>
              </div>
 
-             <div className='flex flex-row justify-center items-center gap-x-[40px]'>
+             <button onClick={toggleMenu}><HiOutlineMenu className="h-[25px] w-[25px]"/></button>
+             
+             <div className='hidden md:flex flex-row justify-center items-center gap-x-[40px]'>
                 <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500'>Home</p>
                 <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500'>About</p>
                 <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500'>Events</p>
@@ -22,7 +29,7 @@ const Navbar = () =>{
                 <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500'>Alumini</p>
              </div>
 
-             <p className='bg-[#1b6535] text-white py-[10px] px-[20px] cursor-pointer hover:bg-[#30854e] rounded-lg transition-all duration-500'>Contact Us</p>
+             <p className='hidden md:block bg-[#1b6535] text-white py-[10px] px-[20px] cursor-pointer hover:bg-[#30854e] rounded-lg transition-all duration-500'>Contact Us</p>
            </div>
         </div>
     )
