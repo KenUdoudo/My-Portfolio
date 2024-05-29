da6574e14c5ab61f759471437365b7ef8cb14b14
Ken Udoudo <kendo302003@gmail.com>
2024-05-29 11:38:42 -0700

    done with navbar for now

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import logo from '../assets/blacklogo.svg';
 import { HiOutlineMenu } from "react-icons/hi";
+import { useState } from 'react';
 const Navbar = () =>{
     const [isOpen, setIsOpen] = useState(false);
 
@@ -14,11 +15,24 @@ const Navbar = () =>{
                 <p className='text-[#1b6535] text-[24px] font-bold'>NACOS MTU</p>
              </div>
 
-             <button onClick={toggleMenu}><HiOutlineMenu className="h-[25px] w-[25px]"/></button>
+             <button onClick={toggleMenu} className='block md:hidden z-30'><HiOutlineMenu className="h-[25px] w-[25px]"/></button>
 
-             <div className={isOpen ? "" : ""}>
-                <p>omo</p>
+             <div className={`fixed top-0 right-0 h-full bg-[#e9e9e9] w-[75%] transform ${isOpen ? '-translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out`}>
+                <div className='flex flex-col items-start gap-y-[20px] px-[20px] py-[20px]'>
+                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Home</p>
+                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>About</p>
+                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Events</p>
+                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Library</p>
+                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>QHub</p>
+                    <div>
+                        <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Resources</p>
+
+                    </div>
+                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Alumini</p>
+                    <p className='bg-[#1b6535] text-white py-[10px] px-[20px] cursor-pointer hover:bg-[#30854e] rounded-lg transition-all duration-500'>Contact Us</p>
+                </div>
              </div>
+
              <div className='hidden md:flex flex-row justify-center items-center gap-x-[40px]'>
                 <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500'>Home</p>
                 <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500'>About</p>
