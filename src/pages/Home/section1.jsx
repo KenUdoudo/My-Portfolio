d6560a1ca9b2168a43d7b39987718c9f7bc0ead8
Ken Udoudo <kendo302003@gmail.com>
2024-05-29 11:51:22 -0700

    omo

--- src/pages/Home/section1.jsx
+++ src/pages/Home/section1.jsx
@@ -7,12 +7,12 @@ const Sec1home = () => {
             <div className='relative'>
             <img src={ bg } className='h-[100vh] w-full object-cover'/>
             
-            <div className='absolute top-[35%] z-10 flex flex-col items-start gap-y-[10px] px-[50px]'>
-                <p className='text-[45px] text-white font-extrabold'>POWERED BY <br />COMPUTER SCIENCE</p>
-                <p className='text-[30px] text-white'>Empowered to Excel</p>
-                <p className='bg-[#1b6535] text-white py-[10px] px-[30px] cursor-pointer hover:bg-[#30854e] rounded-xl transition-all duration-500'>Learn About</p>
+            <div className='absolute top-[30%] md:top-[35%] z-10 flex flex-col items-start gap-y-[10px] px-[20px] md:px-[50px]'>
+                <p className='text-[30px] md:text-[45px] text-white font-extrabold'>POWERED BY <br />COMPUTER SCIENCE</p>
+                <p className='text-[20px] md:text-[30px] text-white'>Empowered to Excel</p>
+                <p className='bg-[#1b6535] text-white text-[13px] md:text-[16px] py-[10px] px-[30px] cursor-pointer hover:bg-[#30854e] rounded-xl transition-all duration-500'>Learn About</p>
             </div>
-            <div className='flex flex-row items-center gap-[15px] absolute bottom-[10px] left-[50px]'>
+            <div className='flex flex-row items-center gap-[15px] absolute bottom-[10px] left-[20px] md:left-[50px]'>
                 <div className='flex flex-row items-center gap-[10px]'>
                     <img src={ greenlogo } className='w-[50px] aspect-w-full'/>
                     <span className='w-[3px] h-[50px] bg-white'></span>
