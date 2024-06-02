d6bcb1e87db8e5e6650a8a1a997be15f838d9bb9
Ken Udoudo <kendo302003@gmail.com>
2024-06-02 11:29:42 -0700

    img

--- src/pages/Alumini/alumini.jsx
+++ src/pages/Alumini/alumini.jsx
@@ -1,3 +1,6 @@
+import one from './assets/1.png'
+import two from './assets/2.png'
+import three from './assets/3.png'
 const Alumini = () => {
     return (
         <div className="h-[500px] relative">
@@ -5,6 +8,9 @@ const Alumini = () => {
             <p className="text-[25px] font-semibold">Alumini is coming soon</p>
             <p>Please check back in some time</p>
             </div>
+            <img src={ one } className='absolute top-[60%] right-[10%]'/>
+            <img src={ two } className='absolute top-[25%] right-[20%]'/>
+            <img src={ three } className='absolute top-[85%] left-[20%]'/>
         </div>
     );
 }
