a5091fbf8ac7f3784467b1fa296b951e3c54c9a5
Ken Udoudo <kendo302003@gmail.com>
2024-06-05 03:06:48 -0700

    done

--- src/pages/Alumini/alumini.jsx
+++ src/pages/Alumini/alumini.jsx
@@ -10,11 +10,11 @@ const Alumini = () => {
             <p className="text-[25px] font-semibold">Alumini is coming soon</p>
             <p>Please check back in some time</p>
             </div>
-            <img src={ one } className='absolute md:top-[60%] md:right-[10%]'/>
-            <img src={ two } className='absolute md:top-[25%] md:right-[20%]'/>
-            <img src={ three } className='absolute md:top-[85%] md:left-[20%]'/>
-            <img src={ four } className='absolute md:bottom-[-100px] md:left-[40%]'/>
-            <img src={ five } className='absolute md:top-[30%] md:left-[30%]'/>
+            <img src={ one } className='absolute top-[80%] md:top-[60%] right-[5%] md:right-[10%]'/>
+            <img src={ two } className='absolute top-[15%] md:top-[25%] right-[10%] md:right-[20%]'/>
+            <img src={ three } className='absolute top-[40%] md:top-[85%] md:left-[20%]'/>
+            <img src={ four } className='absolute bottom-0 md:bottom-[-100px] md:left-[40%]'/>
+            <img src={ five } className='absolute top-[10%] md:top-[30%] left-[15%] md:left-[30%]'/>
         </div>
     );
 }
