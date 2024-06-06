8ece79a5297dfa8e00b6b420ed1c23affdcf1598
Ken Udoudo <kendo302003@gmail.com>
2024-06-06 10:14:35 -0700

    img

--- src/pages/Events/section1.jsx
+++ src/pages/Events/section1.jsx
@@ -4,12 +4,13 @@ const Sec1Event = () => {
             <p className="font-semibold">Upcoming Events</p>
 
             <div className="px-[10px] flex flex-col w-full">
-                <div className="flex flex-row items-center w-full">
-                    <div className="bg-[#d7fec8] w-[70%]">
-
+                <div className="flex flex-row justify-between items-center w-full">
+                    <div className="bg-[#d7fec8] w-[70%] flex flex-row items-center justify-between px-[10px]">
+                        <p><span className="font-bold">Repair Day,</span><br />for all students</p>
+                        <p>LTB, CBAS</p>
                     </div>
-                    <div className="bg-[#d7fec8] w-[20%]">
-
+                    <div className="bg-[#d7fec8] w-[20%] text-center">
+                        <p className="font-bold"><span className="text-[13px] font-normal">Apr</span>31st</p>
                     </div>
                 </div>
             </div>
