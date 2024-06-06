bbc6da72d248086cf33e1cf164b9160b6b571e80
Ken Udoudo <kendo302003@gmail.com>
2024-06-06 02:54:27 -0700

    links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -20,16 +20,16 @@ const Navbar = () =>{
 
              <div className={`fixed top-0 right-0 h-full bg-[#e9e9e9] w-[75%] transform ${isOpen ? '-translate-x-0' : 'translate-x-full'} transition-transform duration-500 ease-in-out overflow-hidden`}>
                 <div className='flex flex-col items-start gap-y-[20px] px-[20px] py-[20px]'>
-                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Home</p>
+                    <Link to="/" className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Home</Link>
                     <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>About</p>
-                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Events</p>
+                    <Link to="/events" className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Events</Link>
                     <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Library</p>
                     <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>QHub</p>
                     <div>
                         <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Resources</p>
 
                     </div>
-                    <p className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Alumini</p>
+                    <Link to="/alumini" className='hover:text-[#e9e9e9] cursor-pointer transition-all duration-500 text-[20px] font-semibold'>Alumini</Link>
                     <p className='bg-[#1b6535] text-white py-[10px] px-[20px] cursor-pointer hover:bg-[#30854e] rounded-lg transition-all duration-500'>Contact Us</p>
                 </div>
              </div>
@@ -37,7 +37,7 @@ const Navbar = () =>{
              <div className='hidden md:flex flex-row justify-center items-center gap-x-[10px] lg:gap-x-[30px] xl:gap-x-[40px]'>
                 <Link to="/" className='hover:text-[#e9e9e9] text-[13px] lg:text-[16px] cursor-pointer transition-all duration-500'>Home</Link>
                 <p className='hover:text-[#e9e9e9] text-[13px] lg:text-[16px] cursor-pointer transition-all duration-500'>About</p>
-                <p className='hover:text-[#e9e9e9] text-[13px] lg:text-[16px] cursor-pointer transition-all duration-500'>Events</p>
+                <Link to="/events" className='hover:text-[#e9e9e9] text-[13px] lg:text-[16px] cursor-pointer transition-all duration-500'>Events</Link>
                 <p className='hover:text-[#e9e9e9] text-[13px] lg:text-[16px] cursor-pointer transition-all duration-500'>Library</p>
                 <p className='hover:text-[#e9e9e9] text-[13px] lg:text-[16px] cursor-pointer transition-all duration-500'>QHub</p>
                 <div>
