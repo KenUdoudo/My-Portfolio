18a06eede01d269858726d9bc13b77a5a0de2b4c
Ken Udoudo <kendo302003@gmail.com>
2024-06-06 03:05:43 -0700

    i think

--- src/pages/Events/section1.jsx
+++ src/pages/Events/section1.jsx
@@ -1,7 +1,18 @@
 const Sec1Event = () => {
     return (  
-        <div>
-            
+        <div className="pt-[70px] px-[20px]">
+            <p className="font-semibold">Upcoming Events</p>
+
+            <div className="px-[10px] flex flex-col w-full">
+                <div className="flex flex-row items-center w-full">
+                    <div className="bg-[#d7fec8] w-[70%]">
+
+                    </div>
+                    <div className="bg-[#d7fec8] w-[20%]">
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
