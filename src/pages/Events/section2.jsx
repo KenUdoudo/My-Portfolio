3b101d2ac4eb02b47d35761a00249e8082ac0fea
Ken Udoudo <kendo302003@gmail.com>
2024-06-07 11:28:10 -0700

    omo

--- src/pages/Events/section2.jsx
+++ src/pages/Events/section2.jsx
@@ -2,6 +2,15 @@ const Sec2Event = () => {
     return (  
         <div className="mt-[40px] px-[20px] md:px-[50px]">
             <p className="font-semibold">Key Events</p>
+
+            <div className="p-[20px] bg-white rounded-lg mt-[20px] flex flex-col">
+                <div className="w-full flex flex-row justify-between">
+                    <div className="flex flex-col justify-start items-start w-[45%]">
+                        <div className="w-full bg-[#e9e9e9] h-[130px] rounded-lg"></div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
