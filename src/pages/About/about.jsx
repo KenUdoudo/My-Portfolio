62e944f3f966faafffca4651431aacd66b01af9b
Ken Udoudo <kendo302003@gmail.com>
2024-06-08 04:23:57 -0700

    importing sec2

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,9 +1,12 @@
 import Sec1About from "./section1";
+import Sec2About from "./section2";
 
 const About = () => {
     return (  
         <div>
             <Sec1About />
+
+            <Sec2About />
         </div>
     );
 }
---
+++ src/pages/About/section2.jsx
@@ -0,0 +1,14 @@
+const Sec2About = () => {
+    return (
+        <div className="flex flex-col-reverse md:flex-row py-[40px] px-[20px] lg:px-[50px]">
+            <div className="text-center">
+                <p>OVER</p>
+                <p>1,000,000</p>
+                <p>REGISTERED MEMBERS</p>
+                <p>2023</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Sec2About;
