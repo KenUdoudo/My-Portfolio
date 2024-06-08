f253f0652b92a069737295e3d685be5428c8f3b3
Ken Udoudo <kendo302003@gmail.com>
2024-06-08 07:19:27 -0700

    almost there

--- src/pages/About/section5.jsx
+++ src/pages/About/section5.jsx
@@ -1,11 +1,80 @@
+import one from './assets/1.png';
+import two from './assets/2.png';
+import three from './assets/3.png';
+import four from './assets/4.png';
+import five from './assets/5.png';
+import six from './assets/6.png';
+import seven from './assets/7.png';
+import eight from './assets/8.png';
+import nine from './assets/9.png';
+import ten from './assets/10.png';
+import eleven from './assets/11.png';
+import twelve from './assets/12.png';
 const Sec5About = () => {
     return (  
-        <div className="px-[20px] lg:px-[50px] mt-[40px] lg:mt-[120px]">
+        <div className="px-[20px] lg:px-[50px] mt-[40px] lg:mt-[120px] font-pop">
             <p className="text-center text-[20px] md:text-[25px] font-semibold">Executive Committee 2023</p>
 
-            <div className="flex flex-row flex-wrap w-full">
-                <div className="flex flex-col justify-start items-start">
-
+            <div className="flex flex-row flex-wrap justify-center gap-[30px] w-full  mt-[20px]">
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ one }/>
+                    <p className='font-semibold md:text-[25px]'>Merit Mohammed</p>
+                    <p className='text-[13px] md:text-[18px]'>President</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ two }/>
+                    <p className='font-semibold md:text-[25px]'>Oluwadamilola Adesina</p>
+                    <p className='text-[13px] md:text-[18px]'>Vice President</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ three }/>
+                    <p className='font-semibold md:text-[25px]'>Oluwayomi Apata</p>
+                    <p className='text-[13px] md:text-[18px]'>General Secretary</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ four }/>
+                    <p className='font-semibold md:text-[25px]'>Daniel Ikemefuna</p>
+                    <p className='text-[13px] md:text-[18px]'>Financial Secretary</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ five }/>
+                    <p className='font-semibold md:text-[25px]'>David Ufot</p>
+                    <p className='text-[13px] md:text-[18px]'>Public Relations Officer</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ six }/>
+                    <p className='font-semibold md:text-[25px]'>Iyiola Ogunleye</p>
+                    <p className='text-[13px] md:text-[18px]'>Assistant General Secretary</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ seven }/>
+                    <p className='font-semibold md:text-[25px]'>Glory Olubanjo</p>
+                    <p className='text-[13px] md:text-[18px]'>Social Director</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ eight }/>
+                    <p className='font-semibold md:text-[25px]'>Moyo Toluhi</p>
+                    <p className='text-[13px] md:text-[18px]'>Sports Director</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ nine }/>
+                    <p className='font-semibold md:text-[25px]'>Grace Nicholas</p>
+                    <p className='text-[13px] md:text-[18px]'>Librarian</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ ten }/>
+                    <p className='font-semibold md:text-[25px]'>Daniel Adedoyin</p>
+                    <p className='text-[13px] md:text-[18px]'>Assistant Librarian</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ eleven }/>
+                    <p className='font-semibold md:text-[25px]'>Oluwaseun Oke</p>
+                    <p className='text-[13px] md:text-[18px]'>Speaker</p>
+                </div>
+                <div className="flex flex-col justify-start items-start w-[40%] aspect-w-full">
+                    <img src={ twelve }/>
+                    <p className='font-semibold md:text-[25px]'>Success Samuel</p>
+                    <p className='text-[13px] md:text-[18px]'>Chairman, Media and Coverage</p>
                 </div>
             </div>
         </div>
