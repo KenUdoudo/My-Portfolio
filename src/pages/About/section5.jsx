3a1229d72a743a8eda23dd153d0346997b8e3060
Ken Udoudo <kendo302003@gmail.com>
2024-06-08 07:37:47 -0700

    omo

--- src/pages/About/section5.jsx
+++ src/pages/About/section5.jsx
@@ -20,82 +20,82 @@ const Sec5About = () => {
             <p className="text-center text-[20px] md:text-[25px] font-semibold">Executive Committee 2023</p>
 
             <div className="flex flex-row flex-wrap justify-center gap-[30px] w-full  mt-[20px]">
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ one }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Merit Mohammed</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>President</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ two }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Oluwadamilola Adesina</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Vice President</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ three }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Oluwayomi Apata</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>General Secretary</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ four }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Daniel Ikemefuna</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Financial Secretary</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ five }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>David Ufot</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Public Relations Officer</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ six }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Iyiola Ogunleye</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Assistant General Secretary</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ seven }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Glory Olubanjo</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Social Director</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ eight }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Moyo Toluhi</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Sports Director</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ nine }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Grace Nicholas</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Librarian</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ ten }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Daniel Adedoyin</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Assistant Librarian</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ eleven }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Oluwaseun Oke</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Speaker</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ twelve }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Success Samuel</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Chairman, Media and Coverage</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ thirteen }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Clinton Obigho</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Chief Whip</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ fourteen }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Kesnia Uchendu</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Clerk</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ fifteen }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Paul Etok</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Chairman, Production and Sales</p>
                 </div>
-                <div className="flex flex-col justify-start items-start w-[40%] lg:w-[20%] aspect-w-full">
+                <div className="flex flex-col justify-start items-start w-[40%] md:w-[30%] lg:w-[20%] aspect-w-full">
                     <img src={ sixteen }/>
                     <p className='font-bold md:text-[25px] lg:text-[18px]'>Kayode Toluwani</p>
                     <p className='text-[13px] md:text-[18px] lg:text-[16px]'>Chairman, Welfare Unit</p>
